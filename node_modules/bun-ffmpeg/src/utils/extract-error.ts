74ba3c02d2e796031e5a6394fc06fd3d09be0340
kaloszer <kaloszer1337@gmail.com>
2024-08-27 21:35:01 +0200

    Remove node_modules directory

--- node_modules/bun-ffmpeg/src/utils/extract-error.ts
+++
@@ -1,6 +0,0 @@
-export function extractError(stderr: string): string {
-  return stderr
-    .split(/\r\n|\r|\n/g)
-    .filter(message => message.charAt(0) !== ' ' && message.charAt(0) !== '[')
-    .join('\n')
-}
